a18eba3cc8f6e4d2169108d9ba55ffd8935c0c13
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-05-25 01:42:30 +0300

    Add basic methods for openbrewery testing

---
+++ src/openbrewery_api_methods.py
@@ -0,0 +1,33 @@
+import requests
+
+
+'https://api.openbrewerydb.org/breweries/'
+
+class OpenBreweryMethods:
+
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def get_single_brewery(self, brewery):
+        url = f'{self.base_url}/{brewery}'
+        response = requests.get(url)
+        response.raise_for_status()
+        return response
+
+    def get_list_of_breweries(self, payload=None):
+        url = f'{self.base_url}'
+        response = requests.get(url, payload)
+        response.raise_for_status()
+        return response
+
+    def search_breweries(self, payload=None):
+        url = f'{self.base_url}/search'
+        response = requests.get(url, payload)
+        response.raise_for_status()
+        return response
+
+    def search_breweries_w_autocomplete(self, payload=None):
+        url = f'{self.base_url}/autocomplete'
+        response = requests.get(url, payload)
+        response.raise_for_status()
+        return response
