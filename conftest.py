a8a31aec192b12b9b14166cd67af4e47947d8773
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-06-03 00:57:31 +0300

    Add jsonplaceholder fixture

--- conftest.py
+++ conftest.py
@@ -1,13 +1,15 @@
 import pytest
 from src import dog_ceo_api_methods
 from src import openbrewery_api_methods
+from src import jsonplaceholder_api_methods
 
 
 @pytest.fixture(scope="module")
 def set_base_urls():
     urls = {
         'dog_ceo_url': 'https://dog.ceo/api',
-        'openbrewery_url': 'https://api.openbrewerydb.org/breweries'
+        'openbrewery_url': 'https://api.openbrewerydb.org/breweries',
+        'jsonplaceholder_url': 'https://jsonplaceholder.typicode.com'
     }
     return urls
 
@@ -42,3 +44,11 @@ def create_openbrewery(set_base_urls):
         set_base_urls['openbrewery_url']
     )
     return openbrewery
+
+
+@pytest.fixture(scope="module")
+def create_jsonplaceholder(set_base_urls):
+    jsonplaceholder = jsonplaceholder_api_methods.JsonPlaceholderMethods(
+        set_base_urls['jsonplaceholder_url']
+    )
+    return jsonplaceholder
