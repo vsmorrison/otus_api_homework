1bcaf6b2695210c5b7c00c44d902df626b11bb37
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-06-03 00:29:54 +0300

    Modify dogceo and openbrewery fixtures

--- conftest.py
+++ conftest.py
@@ -29,40 +29,16 @@ def set_query_params(request):
 
 
 @pytest.fixture(scope="module")
-def get_all_breeds(set_base_urls):
-    dog_ceo_url = dog_ceo_api_methods.DogCeoMethods(set_base_urls['dog_ceo_url'])
-    return dog_ceo_url.get_all_breeds()
+def create_dogceo(set_base_urls):
+    dogceo = dog_ceo_api_methods.DogCeoMethods(
+        set_base_urls['dog_ceo_url']
+    )
+    return dogceo
 
 
 @pytest.fixture(scope="module")
-def get_random_image(set_base_urls):
-    dog_ceo_url = dog_ceo_api_methods.DogCeoMethods(set_base_urls['dog_ceo_url'])
-    return dog_ceo_url.get_random_image()
-
-
-@pytest.fixture(scope="module")
-def get_images_by_breed(set_base_urls, set_breed):
-    dog_ceo_url = dog_ceo_api_methods.DogCeoMethods(set_base_urls['dog_ceo_url'])
-    return dog_ceo_url.get_all_pictures_by_breed(set_breed)
-
-
-@pytest.fixture(scope="module")
-def get_images_by_sub_breed(set_base_urls, set_breed, set_sub_breed):
-    dog_ceo_url = dog_ceo_api_methods.DogCeoMethods(set_base_urls['dog_ceo_url'])
-    return dog_ceo_url.get_image_by_sub_breed(set_breed, set_sub_breed)
-
-
-@pytest.fixture(scope="module")
-def get_single_brewery(set_base_urls, request):
+def create_openbrewery(set_base_urls):
     openbrewery = openbrewery_api_methods.OpenBreweryMethods(
         set_base_urls['openbrewery_url']
     )
-    return openbrewery.get_single_brewery(request.param)
-
-
-@pytest.fixture(scope="module")
-def get_breweries(set_base_urls, set_query_params):
-    openbrewery = openbrewery_api_methods.OpenBreweryMethods(
-        set_base_urls['openbrewery_url']
-    )
-    return openbrewery.get_list_of_breweries(set_query_params)
+    return openbrewery
