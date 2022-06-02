21e54b85c8802f7c6f1398a8d8fcbef67a3c1415
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-06-03 00:30:48 +0300

    Small fixes

--- src/openbrewery_api_methods.py
+++ src/openbrewery_api_methods.py
@@ -9,24 +9,19 @@ class OpenBreweryMethods:
     def get_single_brewery(self, brewery):
         url = f'{self.base_url}/{brewery}'
         response = requests.get(url)
-        response.raise_for_status()
         return response
 
     def get_list_of_breweries(self, payload=None):
         url = f'{self.base_url}'
         response = requests.get(url, params=payload)
-        response.raise_for_status()
         return response
 
     def search_breweries(self, payload=None):
         url = f'{self.base_url}/search'
         response = requests.get(url, params=payload)
-        response.raise_for_status()
         return response
 
     def search_breweries_w_autocomplete(self, payload=None):
         url = f'{self.base_url}/autocomplete'
         response = requests.get(url, params=payload)
-        response.raise_for_status()
         return response
-gi
