2ac148f6a3aad71dd52224808a00d64fb9e3cc45
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-06-04 15:38:46 +0300

    Add cli options and make_get_request method

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from src import dog_ceo_api_methods
 from src import openbrewery_api_methods
 from src import jsonplaceholder_api_methods
+from src import custom_url_methods
 
 
 @pytest.fixture(scope="module")
@@ -52,3 +53,31 @@ def create_jsonplaceholder(set_base_urls):
         set_base_urls['jsonplaceholder_url']
     )
     return jsonplaceholder
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--url",
+        default="https://ya.ru",
+        help="This is request url"
+    )
+    parser.addoption(
+        "--status_code",
+        default=200,
+        help="This is status code"
+    )
+
+
+@pytest.fixture(scope="module")
+def get_url(request):
+    return request.config.getoption("--url")
+
+
+@pytest.fixture(scope="module")
+def get_status_code(request):
+    return request.config.getoption("--status_code")
+
+
+@pytest.fixture(scope="module")
+def make_get_request(get_url):
+    return custom_url_methods.make_get_request(get_url)
